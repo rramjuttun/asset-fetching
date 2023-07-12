c73d0dc4addce20f29811e269dbfbe5bf763c794
rramjuttun <47162900+rramjuttun@users.noreply.github.com>
2023-07-12 14:26:44 -0700

    declaration

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
-declare class Gateway {
+export declare class Gateway {
     constructor(gateway: string, accountAddress?: string, ethURI?: string);
     urlFromCid(ipfsHash: string): URL | null;
     urlFromJsonEntry(jsonList: any[], key: string): Promise<string>;
 }
-  
-export = Gateway;
