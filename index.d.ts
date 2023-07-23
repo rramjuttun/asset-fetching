9585f693e649de077748f1c86f30d6da5bb361a6
rramjuttun <47162900+rramjuttun@users.noreply.github.com>
2023-07-23 13:59:38 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export declare class Gateway {
-    constructor(gateway: string, accountAddress?: string, ethURI?: string);
+    constructor(gateway: Location, accountAddress?: string, ethURI?: string);
     urlFromCid(ipfsHash: string): URL | null;
     urlFromJsonEntry(jsonList: any, key: string): Promise<string>;
 }
